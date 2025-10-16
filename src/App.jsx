01b79d06e47a135f841f8556121f6cb049d9a42f
rsharp99construction <rsharp99@yahoo.co.uk>
2025-10-16 19:18:29 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -150,3 +150,9 @@ export default function App() {
     </div>
   )
 }
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './styles.css'
+
+createRoot(document.getElementById('root')).render(<App />)
